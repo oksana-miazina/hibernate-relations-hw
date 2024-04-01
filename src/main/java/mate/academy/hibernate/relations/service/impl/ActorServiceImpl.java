9422e2f8e7705a2f1a05c26dd04cd815030e60b2
Oksana Miazina <oksana.myazina@gmail.com>
2024-04-01 17:14:21 +0300

    fix: DI in services

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,14 @@
 package mate.academy.hibernate.relations.service.impl;
 
 import mate.academy.hibernate.relations.dao.ActorDao;
-import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
-import org.hibernate.SessionFactory;
 
 public class ActorServiceImpl implements ActorService {
     private ActorDao actorDao;
 
-    public ActorServiceImpl(SessionFactory sessionFactory) {
-        actorDao = new ActorDaoImpl(sessionFactory);
+    public ActorServiceImpl(ActorDao actorDao) {
+        this.actorDao = actorDao;
     }
 
     @Override
