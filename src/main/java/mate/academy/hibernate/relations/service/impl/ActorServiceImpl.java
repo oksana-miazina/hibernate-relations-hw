58c59ae21e89ff703d1b5ddc662fd6e33e688454
Oksana Miazina <oksana.myazina@gmail.com>
2024-04-03 00:24:01 +0300

    fix: add joinTable and joinColumn

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,6 +1,7 @@
 package mate.academy.hibernate.relations.service.impl;
 
 import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
 
@@ -18,6 +19,8 @@ public class ActorServiceImpl implements ActorService {
 
     @Override
     public Actor get(Long id) {
-        return actorDao.get(id).orElse(null);
+        return actorDao.get(id).orElseThrow(
+                () -> new DataProcessingException("Can't get an actor with id: " + id)
+        );
     }
 }
